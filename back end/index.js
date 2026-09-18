ee0ad22d7b7d824cc40a51bc981fd6c74b31a3b8
mythrialampally-13 <mythrialampally@gmail.com>
2026-09-18 12:54:47 +0530

    Update index.js

--- back end/index.js
+++ back end/index.js
@@ -5,6 +5,7 @@ let hrroutes=require('./routes/hr_routes');
 app.use("/api/hr",hrroutes);
 
 //localhost:3000/api/hr/viewemployees
+
 //run the server 
 app.listen(3000,()=>{
     console.log("server listening on port 3000");

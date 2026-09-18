e3641ee27be027f352f753b6f6c4fff2f3929c1c
mythrialampally-13 <mythrialampally@gmail.com>
2026-09-18 12:49:27 +0530

    second commit

--- back end/routes/emp_routes.js
+++ back end/routes/emp_routes.js
@@ -1,2 +1,9 @@
 let express=require('express');
 let router=express.Router();
+router.post("/register",(req,res)=>{
+    res.send("view register page called");
+})
+
+router.post("/login",(req,res)=>{
+    res.send("login router called");
+})
